67291b2325b0252d70440ec8093f7387e78af425
Evan Prothro <798287+eprothro@users.noreply.github.com>
2022-08-25 17:17:22 -0500

    custom error component instead of 500 page

---
+++ pages/_error.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import type { ErrorProps } from "next/error";
+import NextErrorComponent from "next/error";
+
+const CustomErrorComponent: NextPage<ErrorProps> = props => {
+  return (
+    <p> Custom 500 page </p>
+  );
+};
+
+CustomErrorComponent.getInitialProps = async contextData => {
+  // In case this is running in a serverless function, await this in order to give Sentry
+  // time to send the error before the lambda exits
+  console.log("reporting error to error service")
+
+  // This will contain the status code of the response
+  return NextErrorComponent.getInitialProps(contextData);
+};
+
+export default CustomErrorComponent;
