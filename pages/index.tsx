d150bb897f8aa3372d46e6590a315d4a8e78429f
Evan Prothro <798287+eprothro@users.noreply.github.com>
2022-08-25 16:14:33 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+/** Add your relevant code here for the issue to reproduce */
+import { InferGetServerSidePropsType, NextPage } from "next";
+
+export async function getServerSideProps() {
+  console.log('about to throw error from getServerSideProps');
+  throw new Error('a test error has been manually triggered');
+}
+
+const TestServerSideErrorPage: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = () => {
+  return (
+    <>
+      <p>server side error test page</p>
+    </>
+  );
+};
+
+export default TestServerSideErrorPage;
