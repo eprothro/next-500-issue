4c38517f7cccbce5418219a96ea84f00bfa03509
Evan Prothro <798287+eprothro@users.noreply.github.com>
2022-08-29 11:03:35 -0500

    wip

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -11,19 +11,23 @@ const CustomErrorComponent: NextPage<ErrorProps> = props => {
 async function reportError(contextData: NextPageContext) {
   if(contextData.err){
     console.log("reporting error to error service:")
-    console.log(contextData.err.message)
-    console.log(contextData.err.stack)
+    console.log(`  message: ${contextData.err.message}`)
+    console.log(`  stack: ${contextData.err.stack}`)
     console.log("done reporting error.")
   } else {
-    console.log("no error to report:")
-    console.log(contextData.asPath)
-    console.log(contextData.pathname)
+    console.log("An error occurred, but there was no error information in the NextPageContext:")
+    console.log(`  path: ${contextData.asPath}`)
+    console.log(`  page: ${contextData.pathname}`)
+    console.log(`  status: ${contextData.res?.statusCode}`)
   }
 }
 
 CustomErrorComponent.getInitialProps = async contextData => {
   // In case this is running in a serverless function, await this in order to give
   // time to send the error before the lambda exits
+  //
+  // Any 404 will be caught by our custom error page
+  // at pages/404 so if we're here we know we have an error
   await reportError(contextData);
 
 
